8ee3e56ca3a9b5d98fd575ae01f6541cec4ee35f
sapox <zapponi.juan.jose@gmail.com>
2015-03-17 17:44:56 -0300

    first commit

---
+++ prueba.js
@@ -0,0 +1,5 @@
+//every computer program is a set of instructions.
+//self conteined instructions, breaking apart a more complex task into individuals instructions and using a programming
+//languaje to write those instructions
+//rules = syntax
+document.write("<h1>hola che</h1>");
